7600d85fd043e3534f32203213fc9100fb94139d
ljdj <ljdj89@gmail.com>
2019-01-30 01:47:54 +0100

    Ajour de l'xp et ecriture en base

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('database.json');
+const db = low(adapter);
+
+db.defaults({ histoires: [], xp: []}).write()
 
 var prefix = ("*")
 
@@ -13,7 +20,7 @@ bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
   if (message.content === prefix + "help") {
-    message.channel.send("Liste des commandes: \n *burgerbot \n *fabriquant \n *embed");
+    message.channel.send("Liste des commandes: \n *burgerbot \n *fabriquant \n *embed \n *xp");
     console.log("Commande help effectué")
   }
 
@@ -37,5 +44,34 @@ bot.on('message', message => {
       .setFooter("Bon moment parmis nous ! :)")
     message.channel.send(embed);
     console.log("Commande embed effectué");
+  }
+
+  var msgauthor = message.author.id;
+
+  if (message.author.bot)return;
+
+  if (!db.get("xp").find({user: msgauthor}).value()) {
+    db.get("xp").push({user: msgauthor, xp: 1}).write();
+  } else {
+      var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
+      console.log(userxpdb);
+      var userxp = Object.values(userxpdb);
+      console.log(userxp);
+      console.log(`Nombre d'xp: ${userxp[1]}`);
+
+      db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1] += 1}).write();
+
+    if (message.content === prefix + "xp") {
+      var xp = db.get("xp").filter({user: msgauthor}).find('xp').value()
+      var xpfinal = Object.values(xp);
+      var xp_embed = new Discord.RichEmbed()
+        .setTitle(`Stat des XP de ${message.author.username}`)
+        .setColor('#F4D03F')
+        .setDescription("Affiche des XP")
+        .addField("xp", `${xpfinal[1]} xp`)
+        .setFooter("Enjoy :p")
+      message.channel.send({embed: xp_embed});
+      console.log("Commande xp effectué");
+    }
   }
 });
